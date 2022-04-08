d8a1db4972794492550e13bcd1fa26f51d22b7b8
Nikolina Kadieva <nikolina_kadieva@yahoo.com>
2022-04-08 13:41:14 +0300

    Introduced Admin area

---
+++ EstateRentingSystem/Areas/Admin/Controllers/EstatesController.cs
@@ -0,0 +1,9 @@
+﻿namespace EstateRentingSystem.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public abstract class EstatesController : AdminController
+    {
+        public IActionResult Index() => View();
+    }
+}
